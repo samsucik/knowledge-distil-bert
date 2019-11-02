7c3c526d39d42779ff8f5e2e1a6852fed5dde40a
samsucik <sucik.samo@gmail.com>
2019-11-02 15:54:52 +0000

    Make work dataset caching, GPT-2 finetuning and prefix sampling.

--- experiments/run_finetune-gpt2.sh
+++ experiments/run_finetune-gpt2.sh
@@ -48,10 +48,10 @@ echo "GPT-2 FINETUNING STARTING"
 pushd $DBERT
 
 # cache the dataset
-#python -m dbert.generate.cache_datasets \
-#  --data-dir $GLUE_DIR_LOCAL/$TASK_NAME \
-#  --output-file $OUT_DIR/dataset \
-#  --dataset_name $TASK_NAME
+python -m dbert.generate.cache_datasets \
+  --data-dir $GLUE_DIR_LOCAL/$TASK_NAME \
+  --output-file $OUT_DIR/dataset \
+  --dataset_name $TASK_NAME
 
 # finetune gpt2
 python -m dbert.generate.finetune_gpt \
@@ -61,16 +61,23 @@ python -m dbert.generate.finetune_gpt \
   --cache-dir "$cache_dir" \
   --gpt2-model "gpt2-medium" \
   --num-train-epochs 1
-exit 0
+
+echo "CACHE_DIR: $cache_dir"
 # sample prefixes
 python -m dbert.generate.build_sampler \
   --cache-file $OUT_DIR/dataset \
-  --save $OUT_DIR/sampled_prefixes \
-  --model-type gpt2
+  --cache-dir "$cache_dir" \
+  --save "$OUT_DIR/sampled_prefixes" \
+  --model-type "gpt2" \
+  --gpt2-model "gpt2-medium"
 
+# exit 0
 # sample sentences
 python -m dbert.generate.sample_gpt \
   --prefix-file $OUT_DIR/sampled_prefixes \
+  --gpt2-model "gpt2-medium" \
+  --num-samples 800000 \
+  --cache-dir "$cache_dir" \
   > $OUT_DIR/sampled_sentences
 
 echo "GPT-2 FINETUNING FINISHED"
