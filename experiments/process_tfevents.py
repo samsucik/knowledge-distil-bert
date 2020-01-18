cf14849f7d139e2455c7b001071812e89f05f05e
samsucik <sucik.samo@gmail.com>
2020-01-18 12:30:52 +0000

    Add results of BERT lr search and BiLSTM batch size search; add total runtime extraction from tfevent logs.

--- experiments/process_tfevents.py
+++ experiments/process_tfevents.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import argparse, os, csv, re, glob
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--in_dir", default="CoLA.bak/distillation/logs", type=str, required=False,
@@ -19,16 +20,23 @@ for log_dir in existing_log_dirs:
     log_file = list(glob.iglob(args.in_dir + "/" + log_dir + '/**/events.out.*', recursive=True))[0]
     stats = []
     rows = ["step", "mcc"]
+    times = []
     for e in tf.compat.v1.train.summary_iterator(log_file):
+        times.append(e.wall_time)
         for v in e.summary.value:
             if v.tag == "eval_mcc":
                 stats.append([e.step, v.simple_value])
             if v.tag == "config/text_summary":
                 config = str(v.tensor.string_val[0])[12:-2].split(", ")
                 [print(l) for l in config]
+    start_time = np.min(times)
+    end_time = np.max(times)
+    runtime = (end_time - start_time)/3600
+    print("Runtime: {:.2f}h.".format(runtime))
 
     out_file = os.path.join(args.out_dir, log_dir + ".csv")
     with open(out_file, "w", newline="") as f:
+        f.write("{}\n".format(runtime))
         f.write("{}\n".format(",".join(rows)))
         writer = csv.writer(f)
         writer.writerows(stats)
