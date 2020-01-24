e4a453eadcb45f10304ffba8b87eae388b3b5b26
samsucik <sucik.samo@gmail.com>
2020-01-24 22:29:04 +0000

    Small changes to probing runner.

--- experiments/run_probe.sh
+++ experiments/run_probe.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -u
 
-task=CoLA
+task=$1
+cfg=$2
+out_dir=$3
+
 model_type=LSTM
 model_dir=$(pwd)/good-student-lstm-new
 is_student=true
 
-task=CoLA
 model_type=BERT
 model_dir=$(pwd)/teacher-CoLA
 is_student=false
@@ -15,6 +17,7 @@ glue_data_dir=$GLUE_DIR/$task
 
 pushd $TRANSFORMERS/examples > /dev/null
 python probe_model.py \
+    --out_dir=$out_dir \
     --senteval_path=$senteval_path \
     --glue_task=$task \
     --model_type=$model_type \
