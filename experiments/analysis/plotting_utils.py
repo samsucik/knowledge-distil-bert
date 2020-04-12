154299606cb2c44d344c4ed6d530090dbc41c3bc
samsucik <sucik.samo@gmail.com>
2020-04-12 18:11:09 +0200

    Finish writing about model probing results.

--- experiments/analysis/plotting_utils.py
+++ experiments/analysis/plotting_utils.py
@@ -31,26 +31,26 @@ def unify_legend_outside(fig, axs, alternate_lws=[leg_lw], legcols=1, position="
     n_handles_orig = len(handles)
     handles += additional_handles
     for h in additional_handles:
-    	labels.append(h.get_label())
+        labels.append(h.get_label())
     legcols = legcols if legcols > 0 else len(handles)
     if position == "right":
-    	legend = ax.legend(handles, labels, bbox_to_anchor=(1.0, 0.5), loc='center right', 
+        legend = ax.legend(handles, labels, bbox_to_anchor=(1.0, 0.5), loc='center right', 
                bbox_transform=fig.transFigure, ncol=legcols, **leg_kwargs)
     elif position == "bottom":
-    	legend = ax.legend(handles, labels, bbox_to_anchor=(0.5, 0.0), loc='lower center', 
+        legend = ax.legend(handles, labels, bbox_to_anchor=(0.5, 0.0), loc='lower center', 
                bbox_transform=fig.transFigure, ncol=legcols, **leg_kwargs)
     elif position == "top":
-    	legend = ax.legend(handles, labels, bbox_to_anchor=(0.5, 1.0), loc='upper center', 
+        legend = ax.legend(handles, labels, bbox_to_anchor=(0.5, 1.0), loc='upper center', 
                bbox_transform=fig.transFigure, ncol=legcols, **leg_kwargs)
     else:
-    	raise ValueError("Invalid legend position: {}".format(position))
+        raise ValueError("Invalid legend position: {}".format(position))
     for i, legobj in enumerate(legend.legendHandles):
         if i < n_handles_orig:
-        	legobj.set_linewidth(alternate_lws[i % len(alternate_lws)])
+            legobj.set_linewidth(alternate_lws[i % len(alternate_lws)])
     for a in axs.flatten():
-    	leg = a.get_legend()
-    	if leg is not None:
-    		leg.remove()
+        leg = a.get_legend()
+        if leg is not None:
+            leg.remove()
 
 def add_overall_axislabel(fig, label, axis="y", side="left", pad=None):
     fig.add_subplot(111, zorder=0)
@@ -58,6 +58,8 @@ def add_overall_axislabel(fig, label, axis="y", side="left", pad=None):
     ax.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False, length=0)
     if axis == "y":
         ax.set_ylabel(label, labelpad=pad)
+        if side == "right":
+            ax.yaxis.set_label_position("right")
     else:
         ax.set_xlabel(label, labelpad=pad)
     despine(ax)
@@ -81,9 +83,40 @@ def colour_subplot_gaps(fig, c="black"):
     return ax
 
 def make_latex_label(model="BERT", role="student"):
-	return "$\\text{" + model + "}_{\\text{" + ("S" if role == "student" else "T") + "}}$"
+    return "$\\text{" + model + "}_{\\text{" + ("S" if role == "student" else "T") + "}}$"
 
 def new_legend_entry(c="black", label="label", marker=None, markersize=5, lw=3):
-	handle = mlines.Line2D([], [], color=c, marker=marker,
-	                     markersize=markersize, label=label, linewidth=lw)
-	return handle
+    handle = mlines.Line2D([], [], color=c, marker=marker,
+                         markersize=markersize, label=label, linewidth=lw)
+    return handle
+
+def make_probing_axis(ax, probing_task="Length"):
+    yticks_major = np.arange(6)*20
+    yticks_minor = np.arange(10)*10
+    probing_task_idx = task_names.index(probing_task)
+    ax.set_yticks(yticks_major, minor=False)
+    ax.set_yticks(yticks_minor, minor=True)
+    ax.set_ylim(0, 100)
+    xs_baseline = ax.get_xlim()
+    ax.fill_between(xs_baseline, 0, baselines["majority"][probing_task_idx], 
+                    facecolor=baseline_colours["majority"], zorder=0)
+    ax.fill_between(xs_baseline, baselines["human"][probing_task_idx], 100, 
+                    facecolor=baseline_colours["human"], zorder=0)
+    for b in ["majority", "human"]:
+        ax.axhline(baselines[b][probing_task_idx], c=modify_cl(baseline_colours[b], 0.9), zorder=0)
+    ax.set_xlim(*xs_baseline)
+    ax.grid(axis="y", color=grid_colour, which="both", zorder=1)
+    ax.set_axisbelow(True)
+    [s.set_linewidth(1.2) for s in [ax.spines[k] for k in ["top", "bottom"]]]
+    [s.set_linewidth(1.2) for s in [ax.spines[k] for k in ["right", "left"]]]
+
+import matplotlib.transforms as transforms
+def annotate_range(vmin, vmax, label, ax, offset=-0.1, width=-0.1):
+    # y-coordinates in axis coordinates,
+    # x-coordinates in data coordinates
+    trans = transforms.blended_transform_factory(ax.transData, ax.transAxes)
+    y = offset + 0.5 * width
+    x = vmin + 0.5 * (vmax - vmin)
+    ax.text(x, y, label,
+            horizontalalignment='center', verticalalignment='top',
+            clip_on=False, transform=trans)
